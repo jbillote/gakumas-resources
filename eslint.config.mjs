e27f37b6f4cb9ead7df0a53d5c31251e257eb9d7
jbillote <jbillote@gmail.com>
2026-03-09 23:40:37 -0700

    chore: allow self-closing on html void elements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
-  ignores: ['**/node_modules', '**/public', '**/.nuxt', 'app/components/ui/**'],
-})
+export default withNuxt(
+  {
+    ignores: ['**/node_modules', '**/public', '**/.nuxt', 'app/components/ui/**'],
+  },
+  {
+    rules: {
+      'vue/html-self-closing': [
+        'warn',
+        {
+          html: {
+            void: 'any',
+          },
+        },
+      ],
+    },
+  },
+)
